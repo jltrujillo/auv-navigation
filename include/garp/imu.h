4b6b20012e857c47b42fadc5d3c21ed86196a08c
yeiniel <yeiniel@uclv.cu>
2014-01-06 18:00:37 -0500

    Adicionada una estructura de datos para representar una IMU (Unidad de Mediciones Inerciales)

---
+++ include/garp/imu.h
@@ -0,0 +1,28 @@
+/*
+ * Derechos Reservados © 2013 GARP y colaboradores
+ *
+ * Este es un Software Libre; como tal redistribuirlo y/o modificarlo está
+ * permitido, siempre y cuando se haga bajo los términos y condiciones de la
+ * Licencia Pública General GNU publicada por la Free Software Foundation, ya
+ * sea en su versión 2 ó cualquier otra de las posteriores a la misma.
+ *
+ * Este “Programa” se distribuye con la intención de que sea útil, sin embargo
+ * carece de garantía, ni siquiera tiene la garantía implícita de tipo comercial
+ * o inherente al propósito del mismo “Programa”. Ver la Licencia Pública
+ * General GNU para más detalles.
+ *
+ * Se debe haber recibido una copia de la Licencia Pública General GNU con este
+ * “Programa”, si este no fue el caso, favor de escribir a la Free Software
+ * Foundation, Inc., 59 Temple Place – Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef _GARP_IMU_H_
+#define _GARP_IMU_H_
+
+/** Estructura que representa una Unidad de Mediciones Inerciales
+ */
+struct imu {
+
+};
+
+#endif /* _GARP_IMU_H_ */

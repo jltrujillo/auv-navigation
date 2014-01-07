23dbf729a4e2a936d12efe334b66b9bc0e6c2827
Yeiniel Suárez Sosa <yeiniel@gmail.com>
2014-01-06 21:55:11 -0400

    Adicionado un gestor de dispositivo para MTi-G de Xsens

---
+++ src/mti-g.c
@@ -0,0 +1,30 @@
+/* Procesador de Mensajes de MTi-G
+ *
+ * Derechos Reservados © 2013 GARP y colaboradores
+ *
+ * Este es un Software Libre; como tal redistribuirlo y/o modificarlo está
+ * permitido, siempre y cuando se haga bajo los términos y condiciones de la
+ * Licencia Pública General GNU publicada por la Free Software Foundation, ya
+ * sea en su versión 2 ó cualquier otra de las posteriores a la misma.
+ *
+ * Este “Programa” se distribuye con la intención de que sea útil, sin embargo
+ * carece de garantía, ni siquiera tiene la garantía implícita de tipo comercial
+ * o inherente al propósito del mismo “Programa”. Ver la Licencia Pública
+ * General GNU para más detalles.
+ *
+ * Se debe haber recibido una copia de la Licencia Pública General GNU con este
+ * “Programa”, si este no fue el caso, favor de escribir a la Free Software
+ * Foundation, Inc., 59 Temple Place – Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include "garp/mti-g.h"
+
+/*! Procesa un pedido realizado por una MTi-G
+ *
+ * @param fd Descriptor de fichero asociado a la MTi-G.
+ * @param mti_g_ptr Puntero al controlador de estado de la MTi-G.
+ */
+void mti_g_process_request(int fd, struct mti_g * mti_g_ptr)
+{
+
+}
